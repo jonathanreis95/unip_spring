bd837e2a6bffd678b8f524f8a59e19206b467d7b
Jonathan Reis <jhone_1995@hotmail.com>
2019-12-08 11:36:50 -0300

    API das materias

---
+++ src/main/java/br/com/escola/repository/MateriaRepository.java
@@ -0,0 +1,8 @@
+package br.com.escola.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.escola.model.Materia;
+
+public interface MateriaRepository extends JpaRepository<Materia, Long> {
+}
