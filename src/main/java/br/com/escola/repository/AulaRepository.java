464cedecbf6c6ed7a51246eafc2c7efe3e7bbf66
Jonathan Reis <jhone_1995@hotmail.com>
2019-12-18 22:55:08 -0300

    criacao dos arquivos css/js e mudanças no layout

--- src/main/java/br/com/escola/repository/AulaRepository.java
+++ src/main/java/br/com/escola/repository/AulaRepository.java
@@ -1,8 +1,24 @@
 package br.com.escola.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import br.com.escola.model.Aula;
+import br.com.escola.model.Materia;
+import br.com.escola.model.MateriaAluno;
+
 
 public interface AulaRepository extends JpaRepository<Aula, Long> {
+	@Query( "select o from Aula o where materiaAluno in :ids" )
+	List<Aula> findByMateriaAluno(@Param("ids") List<MateriaAluno> materiaAlunoRep);
+	//List<Aula> findByMateriaAluno(List<MateriaAluno> materiaAlunoRep);
+	
+	@Modifying
+	@Query("UPDATE Aula a SET a.presenca = :presenca where a.materiaAluno = :aluno_id")
+	public void fazerChamada(String presenca, Long aluno_id);
+	
 }
--- src/main/java/br/com/escola/repository/MateriaAlunoRepository.java
+++ src/main/java/br/com/escola/repository/MateriaAlunoRepository.java
@@ -1,8 +1,17 @@
 package br.com.escola.repository;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import br.com.escola.model.MateriaAluno;
+import br.com.escola.model.Aula;
+import br.com.escola.model.Materia;
 
 public interface MateriaAlunoRepository extends JpaRepository<MateriaAluno, Long> {
+	//@Query( "select m from MateriaAluno m where materia = :materia" )
+	List<MateriaAluno> findByMateria(Materia materia);
+	
+	//List<MateriaAluno> findByMateria(Materia materia);
 }
